4aa0d2b1ce5463417d8bad63fca0a32b078cf0f2
Dermee9840 <iqbeast.9840@gmail.com>
2023-08-21 18:40:43 +0800

    add blog form

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import Header from '@/components/Header';
+import './globals.css';
+import siteMetadata from '@/data/siteMetadata';
+
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: siteMetadata.title,
+  description: siteMetadata.description,
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+          <div className='flex h-screen flex-col justify-between'>
+            <Header />
+            <main className='mb-auto'>{children}</main>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
